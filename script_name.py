2593f6227e7d155bf23719bfc9aa168e2502f449
sugasakimaika <sakimaikawahaha@gmail.com>
2023-11-18 19:04:54 +0900

    Update python file

--- script_name.py
+++ script_name.py
@@ -1,11 +1,15 @@
-import openai
+import os
+from openai import OpenAI
 
-openai.api_key = "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"  # 実際のAPIキーに置き換えてください
+# APIキーを直接指定する
+api_key = ""
+client = OpenAI(api_key=api_key)
 
-response = openai.Completion.create(
+response = client.completions.create(
     model="text-davinci-003",  # 使用するモデルを指定
     prompt="ここにプロンプトを入力",  # APIに送るプロンプト
     max_tokens=50  # 応答の最大トークン数
 )
 
 print(response.choices[0].text.strip())
+
